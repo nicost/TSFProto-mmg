b10ec19969caa463a105331e075a6fe975304237
Nico Stuurman <nicost@users.noreply.github.com>
2011-01-22 01:33:44 +0000

    Fixed bug in matlab code

--- matlab/matchChannels.m
+++ matlab/matchChannels.m
@@ -5,22 +5,26 @@
 
 function [matched] = matchChannels(data, maxdiff)
 matched = [];
+sizem = [];
 for frame = 1 : max(data(:,5))
     im1pointCh1 = find(data(:,4) == 1 & data(:,5) == frame);
     im1pointCh2 = find(data(:,4) == 2 & data(:,5) == frame);
-    
+    clear dt;
     dt = DelaunayTri(data(im1pointCh2,1:2));
     
     for spot = im1pointCh1'
         pid = nearestNeighbor(dt, data(spot,1:2));
-        pid = pid + im1pointCh2(1) - 1;
+        pid = im1pointCh2(pid);
         % todo: check if there already is a match, if so, take the one
         % closest by
         if (abs (data(pid,1) - data(spot,1)) < maxdiff & abs(data(pid,2) - data(spot,2)) < maxdiff)
             matched = [matched ; data(spot,1) data(spot,2) data(pid,1) data(pid,2) frame];
         end
     end
+    g = size(find(matched(:,5) == frame));
+    sizem = [sizem; g];
 end
+sizem
 
                 
     
