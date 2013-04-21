88b71edc0e16cd8fdc2d628a59c7bf4f137dbbba
Nico Stuurman <nicost@users.noreply.github.com>
2013-04-21 22:28:04 +0000

    Update Makefile, and finished writing - but not testing - first function in TSFUtils

--- tsfutil/TSFUtils.cpp
+++ tsfutil/TSFUtils.cpp
@@ -15,30 +15,44 @@
 #include "TSFException.h"
 #include "TSFUtils.h"
 #include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/wire_format.h>
 
 TSF::SpotList TSFUtils::GetHeaderBinary(std::ifstream* ifs) throw (TSFException)
 {
-   int magic = 0;
-   long offset = 0;
-
-   *ifs >> magic;
-   *ifs >> offset;
-
-   if (offset == 0) 
-   {
-      throw new TSFException("Offset to header was 0");
-   }
-
-   ifs->seekg(offset, std::ios_base::cur);
-
    google::protobuf::io::IstreamInputStream* input = 
       new google::protobuf::io::IstreamInputStream(ifs);
    google::protobuf::io::CodedInputStream* codedInput = 
       new google::protobuf::io::CodedInputStream(input);
 
-   uint32_t mSize;
-   codedInput->ReadVarint32(&mSize);
 
+   int32_t magic;
+   *ifs >> magic;
+
+   if (magic != 0) 
+   {
+      throw new TSFException("Magic number is not 0, is this a tsf file?");
+   }
+
+   int64_t offset = 0;
+   *ifs >> offset;
+
+   if (offset != 0)
+   {
+      throw new TSFException("Offset is 0, can not find header data in this file");
+   }
+
+   ifs->seekg(offset, std::ios_base::cur);
+
+   int32_t mSize = 0;
+   *ifs >> mSize;
+
+   std::string buffer;
+   TSF::SpotList* sl = new TSF::SpotList();
+
+   if (codedInput->ReadString(&buffer, mSize))                           
+      sl->ParseFromString(buffer);
+
+   return *sl;
 }
 
 TSF::SpotList TSFUtils::GetHeaderText(std::ifstream ifs)
